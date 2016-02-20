b7cd66e4f175a3cdd8892609091b67e2b6c0698c
ethanmuller <ethan@heysparkbox.com>
2016-02-19 19:15:43 -0600

    :cow: :speech_balloon: More compliments

--- compliments.js
+++ compliments.js
@@ -1,13 +1,19 @@
 var compliments = [
-  "You got this.",
-  "You are generally well-liked.",
-  "You're a winner.",
-  "A lot of people look up to you.",
-  "I love you.",
-  "You deserve a treat.",
+  "You got this",
+  "You're even more charming in person",
+  "If I could hug you I would",
+  "You're so talented",
+  "Have you been working out? It looks like it",
+  "You are generally well-liked",
+  "You're a winner",
+  "A lot of people look up to you",
+  "I love you",
+  "You deserve a treat",
+  "I'm glad you're here",
   "You look really cute today <3",
-  "You're doing such a good job.",
-  "You're a real treat."
+  "People are happier when you're around",
+  "You're doing such a good job",
+  "You're a real treat"
 ];
 
 module.exports = {
